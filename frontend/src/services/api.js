d68e38775463c736c9034717ea1048d2707fe5d5
Aditi Baburao Yewate <aditiyewate@gmail.com>
2026-09-05 21:49:01 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://agrolink-13.onrender.com',
+  baseURL: 'https://agrolink-13.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },

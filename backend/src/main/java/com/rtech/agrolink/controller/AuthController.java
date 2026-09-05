042fa43ef0010e3f8d501f5bf840b33fa480131f
Aditi Baburao Yewate <aditiyewate@gmail.com>
2026-09-05 20:33:32 +0530

    Update AuthController.java

--- backend/src/main/java/com/rtech/agrolink/controller/AuthController.java
+++ backend/src/main/java/com/rtech/agrolink/controller/AuthController.java
@@ -11,7 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
-
+@CrossOrigin(origins = "https://onrender.com", allowCredentials = "true")
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
